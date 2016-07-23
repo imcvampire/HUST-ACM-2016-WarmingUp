09f3814a3c9bc63a3ec4e0823fc871b0ab0df63f
hoangkien <hoanglieuphuong@gmail.com>
2016-07-23 21:36:35 +0700

    Complete B

--- B.cpp
+++ B.cpp
@@ -1,12 +1,10 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main(void)
 {
 	int n;
 	cin >> n;
-	int m = n/2;
-	int i = m / 2;
-	cout << i * 3;
+	cout << 4 * (n / 6);
 	return 0;
 }
