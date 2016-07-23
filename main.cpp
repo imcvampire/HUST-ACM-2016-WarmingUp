58b645d44916d26dfcb7437a12ad9291b67198ec
Vu Trong Hieu <tset0401@gmail.com>
2016-07-23 22:00:39 +0700

    E

--- main.cpp
+++ main.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <vector>
-#include <cstring>
+#include <list>
 
 using namespace std;
 
+void check(int n, int m){
+    list<int> dList;
+    for(int i = 1; i<= n ;i++){
+        dList.push_back(i);
+    }
+    list<int>::iterator it = dList.begin(), it2;
+    int tem = 0;
 
+    for(int i = 1; i < n; i++){
+        advance(it, n-1);
+        for(int j = 1; j <= m; j++){
+            if(++it == dList.end())
+                it = dList.begin();
+            tem = (*it);
+        }
+        it2 = it;
+        it2--;
+        tem = (*it2);
+        dList.erase(it);
+
+        it = it2;
+        tem = (*it);
+    }
+    cout<<(*it)<<endl;
+}
 int main()
 {
-    int x1, x2, y1, y2;
-    cin>>x1>>y1>>x2>>y2;
-    float d = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-    float d1 = sqrt(x1*x1 + y1*y1);
-    float d2 = sqrt(x2*x2+y2*y2);
-    float p = 0, h = 0;
-
-    if(d1 < d2){
-        d1 = ceil(d1);
-        d2 = floor(d2);
-    }else{
-        d1 = floor(d1);
-        d2 = ceil(d2);
+    int T, n, k;
+    cin>>T;
+    while(T--){
+        cin>>n>>k;
+        check(n,k);
     }
-    int res = 0;
-    if( x1*x2*y1*y2 >= 0 ){
-        res = abs(d1-d2)+1;
-    }else{
-        p = (d+d1+d2)/2;
-        h = 2*sqrt(p*(p-d)*(p-d1)*(p-d2))/d;
-        h = ceil(h);
-        res = d1-h+1+d2-h+1;
-    }
-    cout<<res;
     return 0;
 }
